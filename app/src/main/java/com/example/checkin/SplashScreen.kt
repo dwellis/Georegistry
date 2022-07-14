d54558c259edf3efdc7140d0269b247b2a9970a2
Derek Ellis <derekwellis@outlook.com>
2022-07-14 00:12:53 -0500

    Fix splash screen redirects based on login status

--- app/src/main/java/com/example/checkin/SplashScreen.kt
+++ app/src/main/java/com/example/checkin/SplashScreen.kt
@@ -4,12 +4,33 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.view.WindowManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class SplashScreen : AppCompatActivity() {
+
+    private lateinit var database: DatabaseReference
+    private lateinit var accounts : DatabaseReference
+    private lateinit var account : DatabaseReference
+    var adminFlag : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        database = Firebase.database.reference
+        accounts = Firebase.database.reference.child("accounts")
+        account = accounts.child(Firebase.auth.currentUser?.uid.toString())
+
+
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -19,9 +40,33 @@ class SplashScreen : AppCompatActivity() {
         Handler().postDelayed({
 
             // check to see if logged in and if so, are they an admin
+            if(FirebaseAuth.getInstance().currentUser == null) {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+            } else {
 
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+                accounts.addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        adminFlag = snapshot.child(Firebase.auth.currentUser?.uid.toString()).child("admin").value as Boolean
+
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+
+                    }
+                })
+
+                if(adminFlag) {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+
+
+
             finish()
         }, 3000)
     }
