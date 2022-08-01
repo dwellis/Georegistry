3b29d999f06400a2cc2ff2c0385e51d5e5859e17
Derek Ellis <derekwellis@outlook.com>
2022-07-31 20:21:13 -0500

    splash screen animation

--- app/src/main/java/com/cognizant/checkin/ui/SplashScreen.kt
+++ app/src/main/java/com/cognizant/checkin/ui/SplashScreen.kt
@@ -6,8 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
+import android.view.animation.AnimationUtils
 import androidx.annotation.RequiresApi
 import com.cognizant.checkin.R
+import com.cognizant.checkin.databinding.ActivitySplashScreenBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
@@ -21,22 +23,31 @@ class SplashScreen : AppCompatActivity() {
 
     // a flag for setting later while reading from db
     private var adminFlag : Boolean = false
+    lateinit var binding: ActivitySplashScreenBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
+        // animate the splash screen
+        val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_page)
+        binding.SplashScreenImage.startAnimation(slideAnimation)
+
         Handler().postDelayed({
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }, 3000)
+
+
+
     }
 
     companion object {
